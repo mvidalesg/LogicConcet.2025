4488fd40001f599b5cad62027ff698eb673c3c3f
mvidalesg <miltonvidales77758@correo.itm.edu.co>
2025-02-16 20:12:41 -0500

    Is multiple of  another

---
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,17 @@
+﻿using Shared
+
+    do
+{
+    var a = ConsoleExtension.GetInt("ingrese primer numero : ");
+    var b = ConsoleExtension.GetInt("ingrese segundo numero : ");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{a} es multiplo {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no es multiplo {b}");
+    }
+
+
+} while (true);
