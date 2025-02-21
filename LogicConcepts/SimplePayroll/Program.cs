f9ad33976d87cac665cf620ae740d381ac838038
mvidalesg <miltonvidales77758@correo.itm.edu.co>
2025-02-21 00:04:19 -0500

    simple payroll

---
+++ LogicConcepts/SimplePayroll/Program.cs
@@ -0,0 +1,35 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    
+    var name = ConsoleExtension.GetString("Ingrese nombre................................:");
+    var workHours = ConsoleExtension.GetFloat("Ingrese numero de horas trabajadas:...........:");
+    var hourValue = ConsoleExtension.GetDecimal("Ingrese valor hora ...........................:");
+    var salaryMimimun = ConsoleExtension.GetDecimal("Ingrese valor salario minimo mensual..........:");
+
+
+    
+    var salary = (decimal)workHours * hourValue;
+    if (salary < salaryMimimun)
+    {
+        Console.WriteLine($"Nombre........................................:{name}");
+        Console.WriteLine($"Salario.......................................:{salaryMimimun:C2}");
+
+    }
+    else
+    {
+        Console.WriteLine($"Nombre........................................:{name}");
+        Console.WriteLine($"Salario.......................................:{salary:C2}");
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]i, [N]o? ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
