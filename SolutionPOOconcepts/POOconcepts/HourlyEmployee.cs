1e958fe2cfe8f199c035c1ef5bf88b6c60971e39
mvidalesg <miltonvidales77758@correo.itm.edu.co>
2025-03-02 22:56:08 -0500

    hourly employee

---
+++ SolutionPOOconcepts/POOconcepts/HourlyEmployee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOconcepts;
+
+public class HourlyEmployee : Employee
+{
+    private int _workingHours;
+    private decimal _valueHour;
+    public HourlyEmployee(int id, string firsName, string lastName, bool isActive, Date? bornDate, Date? hireDate, int workingHours, decimal valueHour) : base(id, firsName, lastName, isActive, bornDate, hireDate)
+    {
+        _workingHours = workingHours;
+        _valueHour = valueHour;
+    }
+
+    public int WorkingHours { get => _workingHours; set => _workingHours = ValidateWorkingHours(value); }
+    public decimal ValueHour { get => _valueHour; set => _valueHour = ValidateValueHour(value); }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}\n\t" +
+            $"Working hours..:   {WorkingHours,20:N2}\n\t" +
+            $"Working value.....:{ValueHour,20:C2}\n\t" +
+            $"Working hours...:  {GetValueToPay(),20:C2}";
+    }
+
+
+    public override decimal GetValueToPay()
+    {
+        return ValueHour * WorkingHours;
+    }
+    private int ValidateWorkingHours(int workingHours)
+    {
+        if (workingHours < 0)
+        {
+            throw new Exception($"the working hours: {workingHours}. is not valid.");
+        }
+
+        return workingHours;
+    }
+    private decimal ValidateValueHour(decimal ValueHour)
+    {
+        if (ValueHour < 10000)
+        {
+            throw new Exception($"the value hours: {ValueHour:C2}. is less than $10.000.");
+        }
+
+        return ValueHour;
+    }
+}
--- SolutionPOOconcepts/POOconcepts/Program.cs
+++ SolutionPOOconcepts/POOconcepts/Program.cs
@@ -4,14 +4,18 @@ try
 {
     var secretary = new SalaryEmployee(1,"Sandra", "Martinez", true, new Date(1981, 11, 14), new Date(2022, 12, 14), 2500000);
     var manager = new SalaryEmployee(2, "Fabio", "ochoa", true, new Date(1971, 11, 14), new Date(2002, 12, 14), 11756324.15M);
+    var washer = new HourlyEmployee(3, "jaime", "Betancur", true, new Date(1980, 12, 1), new Date (2020, 4, 20), 178, 10250.56M);
 
-    var employees = new List<Employee>() { secretary, manager };
+    var employees = new List<Employee>() { secretary, manager, washer };
+    var total = 0M;
+
     foreach (var employee in employees)
     {
         Console.WriteLine(employee);
         Console.WriteLine("----------------------------------------------");
-
+        total += employee.GetValueToPay();
     }
+    Console.WriteLine($"TOTAL....................: {total,20:C2}");
 
 }
 catch (Exception ex)
