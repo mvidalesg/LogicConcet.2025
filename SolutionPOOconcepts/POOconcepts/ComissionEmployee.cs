74927d3f0696e8fdebc5586b9cbceeb5f7ee4af5
mvidalesg <miltonvidales77758@correo.itm.edu.co>
2025-03-11 23:58:40 -0500

    comissionemployee

--- SolutionPOOconcepts/POOconcepts/ComissionEmployee.cs
+++ SolutionPOOconcepts/POOconcepts/ComissionEmployee.cs
@@ -4,9 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace POOconcepts
-{
-    class ComissionEmployee
+namespace POOconcepts;
+
+    public class ComissionEmployee : Employee
     {
+        private float _comissionPercentasje;
+        private decimal _sales;
+    private int comissionPercentasje;
+
+    public ComissionEmployee(int id, string firsName, string lastName, bool isActive, Date? bornDate, Date? hireDate, float comissionPercentasje, decimal sales) : base(id, firsName, lastName, isActive, bornDate, hireDate)
+    {
+        ComissionPercentasje = comissionPercentasje;
+        Sales = sales;
     }
-}
+
+    public float ComissionPercentasje { get => _comissionPercentasje; set => _comissionPercentasje = ValidateComissionPercentasje(value); }
+        public decimal Sales { get => _sales; set => _sales = ValidateSales(value); }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}\n\t" +
+            $"Comision %....:   {ComissionPercentasje,20:P2}\n\t" +
+            $"Sales.........:{Sales,20:C2}\n\t" +
+            $"value to pay...:  {GetValueToPay(),20:C2}";
+    }
+
+
+
+    public override decimal GetValueToPay()
+        {
+            return (decimal)_comissionPercentasje * _sales;
+        }
+    private float ValidateComissionPercentasje(float comissionPercentaje)
+    {
+        if (comissionPercentaje < 0 || comissionPercentaje > 0.3)
+        {
+            throw new Exception($"the comission percentaje: {comissionPercentaje:P2}. is less than $0.00.");
+        }
+
+        return comissionPercentasje;
+    }
+    private decimal ValidateSales(decimal sales)
+        {
+            if (Sales < 0)
+            {
+                throw new Exception($"the sales: {sales:C2}. is less than $0.00.");
+            }
+
+            return sales;
+        }
+    }
+
--- SolutionPOOconcepts/POOconcepts/Program.cs
+++ SolutionPOOconcepts/POOconcepts/Program.cs
@@ -5,8 +5,10 @@ try
     var secretary = new SalaryEmployee(1,"Sandra", "Martinez", true, new Date(1981, 11, 14), new Date(2022, 12, 14), 2500000);
     var manager = new SalaryEmployee(2, "Fabio", "ochoa", true, new Date(1971, 11, 14), new Date(2002, 12, 14), 11756324.15M);
     var washer = new HourlyEmployee(3, "jaime", "Betancur", true, new Date(1980, 12, 1), new Date (2020, 4, 20), 178, 10250.56M);
+    var salesPersol1 = new ComissionEmployee(4, "Monica","Gil", true, new Date(1989, 7, 1), new Date(2020, 4, 20), 0.03F, 670000000M);
+    var salesPersol2 = new ComissionEmployee(4, "Henry", "Vasquez", true, new Date(1979, 1, 1), new Date(2020, 4, 20), 0.03F, 59000000M);
 
-    var employees = new List<Employee>() { secretary, manager, washer };
+    var employees = new List<Employee>() { secretary, manager, washer, salesPersol1, salesPersol2 };
     var total = 0M;
 
     foreach (var employee in employees)
